47b621d359f3af09ad310c5ad10e5dac5f3e5038
Konrad Miekus <miekus.konrad@gmail.com>
2025-07-11 09:23:10 +0200

    Add chat history

--- src/app/api/prompt/route.ts
+++ src/app/api/prompt/route.ts
@@ -5,13 +5,46 @@ import { OpenAIEmbeddings } from '@langchain/openai';
 import { ChatOpenAI } from '@langchain/openai';
 import { formatDocumentsAsString } from 'langchain/util/document';
 import { RunnableSequence } from "@langchain/core/runnables";
-import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { ChatPromptTemplate, MessagesPlaceholder } from "@langchain/core/prompts";
 import { StringOutputParser } from '@langchain/core/output_parsers';
 import { LangChainAdapter } from 'ai';
 
 const PINECONE_API_KEY = process.env.PINECONE_API_KEY!;
 const PINECONE_INDEX = process.env.PINECONE_INDEX!;
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY!;
+
+const chat = new ChatOpenAI({
+  openAIApiKey: OPENAI_API_KEY,
+  modelName: 'gpt-3.5-turbo',
+  temperature: 0.2,
+});
+
+const outputParser = new StringOutputParser();
+
+const contextualizeQSystemPrompt =
+  "Given a chat history and the latest user question " +
+  "which might reference context in the chat history, " +
+  "formulate a standalone question which can be understood " +
+  "without the chat history. Do NOT answer the question, " +
+  "just reformulate it if needed and otherwise return it as is."
+
+const contextualizePrompt = ChatPromptTemplate.fromMessages([
+  ['system', contextualizeQSystemPrompt],
+  new MessagesPlaceholder('chat_history'),
+  ['human', '{question}'],
+])
+
+const promptTemplate = ChatPromptTemplate.fromMessages([
+  [
+    "human",
+    `Use the following context to help answer the question. If the answer is not found in the context, feel free to answer based on your own knowledge. Use three sentences maximum and keep the answer concise.
+     Question: {question} 
+     Context: {context}
+     Answer:`,
+  ],
+])
+
+const contextualizeChain = RunnableSequence.from([contextualizePrompt, chat, outputParser]);
 
 const createRetriever = async () => {
   const pinecone = new Pinecone({ apiKey: PINECONE_API_KEY });
@@ -24,19 +57,11 @@ const createRetriever = async () => {
   return vectorStore.asRetriever();
 };
 
-const promptTemplate = ChatPromptTemplate.fromMessages([
-  [
-    "human",
-    `Use the following context to help answer the question. If the answer is not found in the context, feel free to answer based on your own knowledge. Use three sentences maximum and keep the answer concise.
-     Question: {question} 
-     Context: {context}
-     Answer:`,
-  ],
-]);
 
 export async function POST(req: NextRequest) {
   try {
     const { messages } = await req.json();
+    console.log('messages', messages);
 
     const lastMessage = messages[messages.length - 1];
     if (!lastMessage || lastMessage.role !== 'user') {
@@ -46,19 +71,23 @@ export async function POST(req: NextRequest) {
 
     const retriever = await createRetriever();
 
+    let contextualizedPrompt = null
+
+    if (messages.length > 1) {
+      const chatHistory = messages.slice(0, -1).map((msg) => ({ role: msg.role, content: msg.content }));
+      contextualizedPrompt = await contextualizeChain.invoke({
+        question: prompt,
+        chat_history: chatHistory,
+      });
+
+      console.log('contextualizedPrompt', contextualizedPrompt);
+    }
+
     const retrievalChain = RunnableSequence.from([
       (input) => input.question,
       retriever,
       formatDocumentsAsString,
     ]);
-
-    const chat = new ChatOpenAI({
-      openAIApiKey: OPENAI_API_KEY,
-      modelName: 'gpt-3.5-turbo',
-      temperature: 0.2,
-    });
-
-    const outputParser = new StringOutputParser();
 
     const generationChain = RunnableSequence.from([
       {
@@ -70,7 +99,7 @@ export async function POST(req: NextRequest) {
       outputParser,
     ]);
 
-    const stream = await generationChain.stream({ question: prompt });
+    const stream = await generationChain.stream({ question: contextualizedPrompt || prompt });
 
     return LangChainAdapter.toDataStreamResponse(stream);
   } catch (error) {
